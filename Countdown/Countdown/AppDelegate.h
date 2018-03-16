66225acd313a77cb220372a8d9e16d1cbf9d7774
DukeLenny <1637549689@qq.com>
2018-03-16 15:54:54 +0800

    创建工程.

---
+++ Countdown/Countdown/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Countdown
+//
+//  Created by LiDinggui on 2018/3/16.
+//  Copyright © 2018年 DAQSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
